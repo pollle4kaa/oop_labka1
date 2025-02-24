7a93631022699176b07c09c2bd23598030e1da93
Polina Sitnikova <sitpo123@mail.ru>
2025-02-24 16:53:06 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int plus(int a, int b);
+int minus(int a, int b);
+
+int main() {
+	int x, y;
+	std::cin >> x >> y;
+	std::cout << "plus: " << plus(x,y) << std::endl;
+	std::cout << "minus: " << minus(x, y) << std::endl;
+}
