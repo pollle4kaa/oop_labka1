f9120cb943df7e1cbacf49e6acecde39e92fbbe3
Polina Sitnikova <sitpo123@mail.ru>
2025-02-24 17:03:03 +0300

    fix: Update main output message in main branch

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,6 @@ int minus(int a, int b);
 int main() {
 	int x, y;
 	std::cin >> x >> y;
-	std::cout << "plus: " << plus(x,y) << std::endl;
-	std::cout << "minus: " << minus(x, y) << std::endl;
+	std::cout << "rezultat plus: " << plus(x,y) << std::endl;
+	std::cout << "rezultat minus: " << minus(x, y) << std::endl;
 }
